332f05769bfe36e4f5e7af0fde1265a578a872d6
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2019-02-12 21:11:49 +1100

    Initial commit

---
+++ Assets/Scripts/CharHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharHandler : MonoBehaviour
+{
+    public bool alive;
+
+
+
+
+    private void Start()
+    {
+
+        // Player starts alive
+        alive = true;
+    }
+
+
+
+
+    public void Dead()
+    {
+        // Deactivate the player
+        gameObject.SetActive(false);
+    }
+
+}
+
